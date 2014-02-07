a8473401d85aee433810f2a4740bead5b73a4645
Nick Hirakawa <nh0815@gmail.com>
2014-02-06 22:56:21 -0500

    PyCrawler has limited url opening. Basic interaction with LinkParser.

--- src/PyCrawler.py
+++ src/PyCrawler.py
@@ -1,1 +1,21 @@
 __author__ = 'Nick'
+
+from urllib2 import *
+from LinkParser import LinkParser
+
+def main():
+    parser = LinkParser()
+    parser.feed('<a href="www.google.com">Data</a>')
+    parser.feed('<a  href = "www.facebook.com"></a>')
+    parser.close()
+    for url in parser.get_links():
+        open_url('http://' + url)
+
+def crawl(seed):
+
+
+def open_url(url):
+    request = urlopen(url)
+    print request.readlines()
+
+main()
