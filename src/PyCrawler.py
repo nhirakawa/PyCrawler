e1259eb62a04fb435d63196fc7c5152fc8bb0615
Nick Hirakawa <nh0815@gmail.com>
2014-02-09 03:02:11 -0500

    Crawler does some breadth-first search. Most of the url handling has been implemented. Still some issues with handling relative http paths.

--- src/PyCrawler.py
+++ src/PyCrawler.py
@@ -1,58 +1,76 @@
 __author__ = 'Nick'
 
+from urllib import *
 from urllib2 import *
 from LinkParser import LinkParser
 from argparse import *
 from re import *
+from robotparser import *
+from urlparse import *
+
 
 class PyCrawler():
 
-    def __init__(self, seed, filetypes=['html'], wait=5):
+    def __init__(self, seed, filetypes=['html'], wait=5000):
         self.seed = str(seed)
         self.parser = LinkParser()
-        self.regex = re.compile(build_regex(filetypes))
+        self.regex = build_filetype_regex(filetypes)
         self.wait = wait
+        self.robot_parser = RobotFileParser()
 
     def crawl(self):
-        seen = set([])
-        def breadth_first(url):
-            url = pad_url(url)
+        seen = []
+        frontier = [self.seed]
+        while len(frontier) > 0:
+            url = frontier.pop(0)
+            self.robot_parser.set_url(url)
             if url in seen:
                 return
-            else:
-                html = open_url(url)
+            if self.robot_parser.can_fetch('*', url):
+                host, html = read_html(url)
                 self.parser.feed(html)
                 links = self.parser.get_links()
-                print map(lambda s: url+'/'+s, links)
-        breadth_first(self.seed)
+                http_links = [resolve_relative_path(url, x) for x in links if self.regex.search(x)]
+                for link in http_links:
+                    if link not in frontier:
+                        frontier.append(link)
+                seen.append(url)
+                print frontier
         return seen
 
-def open_url(url):
-    request = urlopen(url)
-    return ''.join(request.readlines())
 
-def regex(file):
-    match = re.search(".(pdf|html)$", file)
-    if match:
-        print file
+def read_html(url):
+    request = Request(url)
+    html = ''.join(urlopen(request).readlines())
+    host = request.get_host()
+    return host, html
 
-def pad_url(url):
-    if re.match('http://',url):
+
+def pad_url(url, host):
+    if re.match("http://", url):
         return url
     else:
-        return 'http://'+url
+        return host + '/' + url
 
-def build_regex(filetypes):
+
+def resolve_relative_path(host, url):
+    if re.match('../', url):
+        return urljoin(host, url[2:])
+    return urljoin(host, url)
+
+
+def build_filetype_regex(filetypes):
     s = r'.('
     for f in filetypes:
         s += f + '|'
     s = s[:-1]
     s += ')$'
-    return s
+    return re.compile(s)
+
 
 def main():
-    pc = PyCrawler('ciir.cs.umass.edu')
-    pc.crawl()
+    pc = PyCrawler('http://ciir.cs.umass.edu/about')
+    print pc.crawl()
 
 
 
