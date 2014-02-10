c36c034b7f7be030b960e3c1b35060913943ed59
nh0815 <nh0815@gmail.com>
2014-02-10 12:39:04 -0500

    Added limit parameter to PyCrawler constructor. Added limit check to search. Added function to figure out robots.txt url (not in use yet). Made resolving relative links more robust (still not great but it works).

--- src/PyCrawler.py
+++ src/PyCrawler.py
@@ -11,31 +11,34 @@ from urlparse import *
 
 class PyCrawler():
 
-    def __init__(self, seed, filetypes=['html'], wait=5000):
+    def __init__(self, seed, filetypes=['html'], wait=5000, limit=10):
         self.seed = str(seed)
         self.parser = LinkParser()
         self.regex = build_filetype_regex(filetypes)
         self.wait = wait
         self.robot_parser = RobotFileParser()
+        self.limit = limit
 
     def crawl(self):
         seen = []
+        print 'seen: %s' % seen
+        print type(seen)
         frontier = [self.seed]
-        while len(frontier) > 0:
+        while len(frontier) > 0 and seen.__len__() < self.limit:
             url = frontier.pop(0)
             self.robot_parser.set_url(url)
-            if url in seen:
-                return
-            if self.robot_parser.can_fetch('*', url):
+            if url not in seen and self.robot_parser.can_fetch('*', url):
+                print 'crawling %s' % url
                 host, html = read_html(url)
                 self.parser.feed(html)
                 links = self.parser.get_links()
                 http_links = [resolve_relative_path(url, x) for x in links if self.regex.search(x)]
+                print http_links
                 for link in http_links:
                     if link not in frontier:
                         frontier.append(link)
                 seen.append(url)
-                print frontier
+        print 'seen: %s' % seen
         return seen
 
 
@@ -53,10 +56,22 @@ def pad_url(url, host):
         return host + '/' + url
 
 
+def get_robots_url(url):
+    host = urlparse(url).netloc
+    return host + '/robots.txt'
+
+
 def resolve_relative_path(host, url):
+    print 'url: %s' % url
+    print 'host: %s' % host
     if re.match('../', url):
-        return urljoin(host, url[2:])
-    return urljoin(host, url)
+        result = urljoin(host, url[2:])
+        return result
+    if not re.search('html$', host):
+        host += '/'
+    result = urljoin(host, url)
+    print result
+    return result
 
 
 def build_filetype_regex(filetypes):
@@ -70,7 +85,9 @@ def build_filetype_regex(filetypes):
 
 def main():
     pc = PyCrawler('http://ciir.cs.umass.edu/about')
-    print pc.crawl()
+    links = pc.crawl()
+    print links
+    print links.__len__()
 
 
 
