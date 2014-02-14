aba83bcdd42804ef23d62c082d71fb8f68b5e43a
Nick Hirakawa <nh0815@gmail.com>
2014-02-14 00:31:15 -0500

    Refactored LinkParser into main file.

--- src/PyCrawler.py
+++ src/PyCrawler.py
@@ -1,11 +1,30 @@
 __author__ = 'Nick'
 
 from urllib2 import *
-from LinkParser import LinkParser
 from re import *
 from robotparser import *
 from urlparse import *
 from time import sleep
+from HTMLParser import HTMLParser
+
+class LinkParser(HTMLParser):
+
+    def __init__(self):
+        HTMLParser.__init__(self)
+        self.links = []
+
+    def feed(self, data):
+        HTMLParser.feed(self, data)
+
+    def handle_starttag(self, tag, attrs):
+        for attr in attrs:
+            if 'href' in attr:
+                self.links.append(attr[1])
+
+    def get_links(self):
+        result = self.links
+        self.links = []
+        return result
 
 
 class PyCrawler():
